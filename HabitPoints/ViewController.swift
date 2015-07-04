14ba8303404c0001c98e6f489a2c651192b077f5
Kenji Crosland <kenji.crosland@gmail.com>
2015-07-04 13:33:45 -0700

    Initial Commit

---
+++ HabitPoints/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HabitPoints
+//
+//  Created by Kenji Crosland on 7/4/15.
+//  Copyright (c) 2015 Kenji Crosland. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
